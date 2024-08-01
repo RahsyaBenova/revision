a5bf57249b24895440bb66d8cc8ab1b2ea81abb5
RahsyaBenova <111744954+RahsyaBenova@users.noreply.github.com>
2024-08-02 06:48:21 +0700

    Feat: sweetalert pada crud page keuangan

--- app/page/golongan/hapus.php
+++ app/page/golongan/hapus.php
@@ -6,8 +6,8 @@ $jabatanInstance = Golongan::getInstance();
 $success = $jabatanInstance->delete($id);
 
 if ($success) {
-    echo "<script>window.location.href = 'index.php?page=data-golongan';</script>";
+    echo "<script>window.location.href = 'index.php?page=data-golongan&&msg=3';</script>";
 } else {
-    echo "<script>window.location.href = 'index.php?page=data-golongan';</script>";
+    echo "<script>window.location.href = 'index.php?page=data-golongan&&msg=3';</script>";
 }
 ?>
--- app/page/jabatan/index.php
+++ app/page/jabatan/index.php
@@ -144,7 +144,6 @@ if($_SESSION['level'] == "common_user") {
     <!-- /.modal-dialog -->
   </div>
 </div>
-<!-- Include SweetAlert2 CSS and JS files if not already included -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
